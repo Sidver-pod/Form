b26425c9c965e98d7d44483bc9de71540aca417d
Sidver-pod <75096302+Sidver-pod@users.noreply.github.com>
2021-10-09 02:02:19 +0530

    Updated local storage to be scalable
    # Local Storage now stores objects in the form of strings. In order to retrieve information they are parsed (string) so as to retain the object back. This allows for a storage that is scalable!
    # Name & Email are put into one object; so only one KEY is required => user1, user2, etc.

--- Form.js
+++ Form.js
@@ -12,10 +12,10 @@
 const btn = document.querySelector('.but');
 btn.style.background = 'red';
 
-btn.addEventListener('click', (e) => {
-  e.preventDefault();
-  console.log(e.target.className);
-});
+// btn.addEventListener('click', (e) => {
+//   e.preventDefault();
+//   console.log(e.target.className);
+// });
 
 btn.addEventListener('mouseover', (e) => {
   e.preventDefault();
@@ -33,30 +33,53 @@ const myForm = document.querySelector('._form');
 const nameInput = document.querySelector('#username');
 const emailInput = document.querySelector('#email');
 
-myForm.addEventListener('click', onSubmit);
+// myForm.addEventListener('click', onSubmit);
 
-function onSubmit(e){
-  e.preventDefault();
-  console.log(nameInput.value);
-  console.log(emailInput.value);
-}
+// function onSubmit(e){
+//   e.preventDefault();
+//   console.log(nameInput.value);
+//   console.log(emailInput.value);
+// }
 
-myForm.addEventListener('click', subTest);
+// myForm.addEventListener('click', subTest);
 
-function subTest(e){
-  e.preventDefault();
-  console.log(nameInput.value);
-  console.log(emailInput.value);  
-}
+// function subTest(e){
+//   e.preventDefault();
+//   console.log(nameInput.value);
+//   console.log(emailInput.value);  
+// }
 
 /*-------------------------STORING IN [LOCAL STORAGE]-------------------------*/
 
-var count = 1; //initialized for the very first user
+var count = 1; //initialized for the very first user (helps make unique KEYS)
+//METHOD #1
+// btn.addEventListener('click', (e) => {
+//   var uniqueName = 'name' + count;
+//   var uniqueEmail = 'email' + count;
+//   localStorage.setItem(uniqueName, nameInput.value);
+//   localStorage.setItem(uniqueEmail, emailInput.value);
+//   count++; //update
+// });
 
+//METHOD #2
+//STORING IN OBJECT FORM (SCALABLE WAY OF STORING)
 btn.addEventListener('click', (e) => {
-  var uniqueName = 'name' + count;
-  var uniqueEmail = 'email' + count;
-  localStorage.setItem(uniqueName, nameInput.value);
-  localStorage.setItem(uniqueEmail, emailInput.value);
+  let userName = nameInput.value;
+  let userEmail = emailInput.value;
+  
+  let myObj = {
+    name: userName,
+    email: userEmail
+  }; //object
+  
+  let uniqueUser = 'user' + count; //KEY
+  let myObj_serialized = JSON.stringify(myObj); //VALUE (converts to a 'string')
+  localStorage.setItem(uniqueUser, myObj_serialized);
   count++; //update
+  myObj_deserialized = JSON.parse(localStorage.getItem(uniqueUser));
+  console.log(myObj_deserialized);
 });
+
+//READING SAVED DATA FROM LOCAL STORAGE INTO THE FRONTEND
+nameInput.value = JSON.parse(localStorage.getItem('user1')).name;
+emailInput.value = JSON.parse(localStorage.getItem('user1')).email;

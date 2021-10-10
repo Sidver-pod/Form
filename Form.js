7eac229130f47dccf4745b95b7331e2484376fea
Sidver-pod <75096302+Sidver-pod@users.noreply.github.com>
2021-10-10 15:08:41 +0530

    Cleared Bug,
    #1 Bug - when the same email, which already exists in the Local Storage, is submitted the new input is registered and the old one is deleted!
    #2 Added Edit/Delete buttons
    #3 Added Edit/Delete functionality

--- Form.js
+++ Form.js
@@ -64,8 +64,29 @@ const emailInput = document.querySelector('#email');
 //METHOD #2
 //STORING IN OBJECT FORM (SCALABLE WAY OF STORING)
 btn.addEventListener('click', (e) => {
+  e.preventDefault();
+  
   let userName = nameInput.value;
   let userEmail = emailInput.value;
+  
+  //check if the email already exists in the Local Storage (BUG)
+  Object.keys(localStorage).every((key) => {
+    var flag = 0;
+    var obj = JSON.parse(localStorage.getItem(key));
+    if(obj.email.toLowerCase() == userEmail.toLowerCase()){
+      var list_Element = document.querySelector('ul');
+      Array.from(list_Element).every((list) => {
+        if(list.innerText.toLowerCase().indexOf(userEmail.toLowerCase()) != -1){
+          list.remove(); //removes from list element (thereby disappears from the FRONT-END)
+          flag = 1;
+          return false; //***instead of forEach(), the every() helps break out by returning false!
+        }
+      });
+    }
+    if(flag == 1){
+      return false; //breaking out of every()!
+    }
+  });
   
   let myObj = {
     name: userName,
@@ -95,7 +116,52 @@ document.addEventListener('DOMContentLoaded', () => {
     var parsed_Value = JSON.parse(stringified_Value);
 
     var li = document.createElement('li');
-    li.innerText = parsed_Value.name + " | " + parsed_Value.email;
+    li.innerText = parsed_Value.name + " | " + parsed_Value.email + " ";
+    li.className = 'details';
+    li.id = 'user' + parsed_Value.email; //key can be used for delete button
+    
+    //UPDATE - ADDING BUTTONS
+    var edit = document.createElement('input');
+    edit.value = 'edit';
+    edit.name = 'edit';
+    edit.className = 'e-d-butn edit';
+    edit.type = 'submit';
+    
+    var del = document.createElement('input');
+    del.value = 'delete';
+    del.name = 'delete';
+    del.className = 'e-d-butn del';
+    del.type = 'submit';
+    
+    li.appendChild(edit);
+    li.appendChild(del);
+    
     ul.appendChild(li);
   }); //for loop ends here
+  
+  //------------------DELETE & EDIT------------------
+  ul.addEventListener('click', (e) => {
+    e.preventDefault();
+    
+    if(e.target.className.slice(9, 12) == 'del'){
+      //*DELETE BUTTON
+      var userDetail = e.target.parentElement;
+      
+      //delete from Local Storage
+      localStorage.removeItem(userDetail.id); //id = key
+      console.log(1);
+      //delete from FRONT-END
+      userDetail.remove();
+    }
+    else if(e.target.className.slice(9, 13) == 'edit'){
+      //*EDIT BUTTON
+      e.target.parentElement.style.display = "none"; //hid it
+      nameInput.value = JSON.parse(localStorage.getItem(e.target.parentElement.id)).name;
+      emailInput.value = JSON.parse(localStorage.getItem(e.target.parentElement.id)).email;
+      localStorage.removeItem(e.target.parentElement.id); //deleted from the Local Storage
+      e.target.parentElement.remove(); //removing the user detail list from FRONT-END
+      //**the rest of the edit functionality works like adding a new user detail list!
+    }
+  });
+  
 }); //DOMContentLoaded ends here
